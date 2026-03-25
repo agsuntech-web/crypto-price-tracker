90662417c6ec5b0caacb3d33ad9fe077f8b6e7c6
SuN tEcH <sunnypacross@gmail.com>
2026-03-24 17:24:40 -0700

    UI upgrade + animations crypto features

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ async function getPrice() {
     return;
   }
 
-  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd`;
-
+  const url = `https://api.coingecko.com/api/v3/coins/${coin}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
@@ -28,3 +27,32 @@ async function getPrice() {
     alert("Error fetching price");
   }
 }
+
+document.getElementById("coinPrice").textContent =
+  `$${data[coin].usd}`;
+
+  document.getElementById("coinChange").textContent =
+  "24h Change: " + data.market_data.price_change_percentage_24h + "%";
+
+  const change = data.market_data.price_change_percentage_24h;
+const changeEl = document.getElementById("coinChange");
+
+changeEl.textContent = `24h Change: ${change}%`;
+
+if (change > 0) {
+  changeEl.style.color = "lightgreen";
+} else {
+  changeEl.style.color = "red";
+}
+
+const elements = document.querySelectorAll(".hidden");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+
+    if (position < window.innerHeight - 50) {
+      el.classList.add("show");
+    }
+  });
+});
