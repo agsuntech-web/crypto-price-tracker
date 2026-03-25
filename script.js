e310cde250387fbe2a70402bc2dcccff3dde87cc
SuN tEcH <sunnypacross@gmail.com>
2026-03-24 17:41:10 -0700

    Upgrade crypto app UI API

--- script.js
+++ script.js
@@ -2,57 +2,34 @@ async function getPrice() {
   const coin = document.getElementById("coinInput").value.trim().toLowerCase();
 
   if (!coin) {
-    alert("Enter a coin like bitcoin or ethereum");
+    alert("Enter a coin name");
     return;
   }
 
   const url = `https://api.coingecko.com/api/v3/coins/${coin}`;
+
   try {
     const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Coin not found");
+    }
+
     const data = await response.json();
 
-    console.log(data);
+    document.getElementById("coinName").textContent = data.name;
 
-    if (!data[coin]) {
-      alert("Coin not found");
-      return;
-    }
-
-    document.getElementById("coinName").textContent = coin.toUpperCase();
     document.getElementById("coinPrice").textContent =
-      "$" + data[coin].usd;
+      "$" + data.market_data.current_price.usd;
+
+    const change = data.market_data.price_change_percentage_24h;
+
+    const changeEl = document.getElementById("coinChange");
+    changeEl.textContent = `24h Change: ${change}%`;
+
+    changeEl.style.color = change > 0 ? "lightgreen" : "red";
 
   } catch (error) {
-    console.log(error);
-    alert("Error fetching price");
+    alert(error.message);
   }
-}
-
-document.getElementById("coinPrice").textContent =
-  `$${data[coin].usd}`;
-
-  document.getElementById("coinChange").textContent =
-  "24h Change: " + data.market_data.price_change_percentage_24h + "%";
-
-  const change = data.market_data.price_change_percentage_24h;
-const changeEl = document.getElementById("coinChange");
-
-changeEl.textContent = `24h Change: ${change}%`;
-
-if (change > 0) {
-  changeEl.style.color = "lightgreen";
-} else {
-  changeEl.style.color = "red";
-}
-
-const elements = document.querySelectorAll(".hidden");
-
-window.addEventListener("scroll", () => {
-  elements.forEach(el => {
-    const position = el.getBoundingClientRect().top;
-
-    if (position < window.innerHeight - 50) {
-      el.classList.add("show");
-    }
-  });
-});
+}    
